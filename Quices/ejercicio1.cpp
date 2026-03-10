b41684a1ddbd208e271a9232bcb4aac2fce84e05
Emiliano Marín Jimenez <nanomarinjimenez12@gmail.com>
2026-03-10 09:15:15 -0600

    Subiendo
    Signed-off-by: Emiliano Marín Jimenez <nanomarinjimenez12@gmail.com>

---
+++ Quices/ejercicio1.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    int precio_base = 0;
+    int recargo = 0;
+    string categoria;
+    cout << "Ingrese su edad: ";
+    cin >> a;
+
+    if (a < 12)
+    {
+        precio_base = 2500;
+        categoria = "Menor de edad";
+    }
+    else if (64 >= a >= 12)
+    {
+        precio_base = 4000;
+        categoria = "Adulto";
+    }
+    else if ( a >= 65)
+    {
+        precio_base = 2000;
+        categoria = "Adulto Mayor";
+    }
+    cout << "Ingrese 1 para dia entre semana o 2 para fin de semana: ";
+    cin >> b;
+
+    if (b == 2)
+    {
+        recargo = (precio_base /100)*20;
+        int total = recargo + precio_base;
+        cout << "Categoria: " << categoria << '\n' << "Precio Base: " << precio_base << '\n' << "Recargo: " << recargo << '\n' << "Total: " << total;
+    }
+    
+    else if (b == 1)
+    {
+        cout << "Categoria: " << categoria << '\n' << "Precio Base: " << precio_base << '\n' << "Recargo: " << recargo << '\n' << "Total: " << precio_base;
+    }
+    
+
+    return 0;
+}
